30569a240523def38243825df0d4fc4d7540a868
absolute11 <den.gitelman@gmail.com>
2023-09-05 20:02:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        // 1. Найти количество несовершеннолетних
+        long underageCount = persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
+        System.out.println("Количество несовершеннолетних: " + underageCount);
+
+        // 2. Получить список фамилий призывников
+        List<String> conscripts = persons.stream()
+                .filter(person -> person.getSex() == Sex.MAN && person.getAge() >= 18 && person.getAge() <= 27)
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+        System.out.println("Список фамилий призывников: " + conscripts);
+
+        // 3. Получить отсортированный по фамилии список потенциально работоспособных людей с высшим образованием
+        List<Person> potentialWorkers = persons.stream()
+                .filter(person -> (person.getSex() == Sex.WOMAN && person.getAge() >= 18 && person.getAge() <= 60) ||
+                        (person.getSex() == Sex.MAN && person.getAge() >= 18 && person.getAge() <= 65))
+                .filter(person -> person.getEducation() == Education.HIGHER)
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+
+        System.out.println("Отсортированный список потенциально работоспособных людей с высшим образованием: ");
+        potentialWorkers.forEach(System.out::println);
+    }
+}
+
+
+
+
+
